b21b04e39eca44b83331e4dbd6bac3b2ff7d8708
alessandroerathos <alessandro.champoski@erathos.com>
2024-08-21 18:54:41 -0300

    [FEATURE] AIRFLOW inicialization

---
+++ dags/your_dags.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+# Função que será executada pela DAG
+def print_hello_world():
+    print("Hello World")
+
+# Definição da DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'hello_world_dag',
+    default_args=default_args,
+    description='A simple Hello World DAG',
+    schedule_interval='@daily',  # A DAG será executada diariamente
+    start_date=days_ago(1),
+    catchup=False,
+)
+
+# Definição da tarefa
+hello_world_task = PythonOperator(
+    task_id='print_hello_world_task',
+    python_callable=print_hello_world,
+    dag=dag,
+)
+
+hello_world_task
